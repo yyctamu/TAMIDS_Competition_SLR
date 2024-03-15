a5c92580c2c1e0362bf6bc75f0b00a1b8a37629e
GageHoweGK <howe.gaged@gmail.com>
2024-03-15 14:11:11 -0500

    Update netcdf4.py

--- netcdf4.py
+++ netcdf4.py
@@ -1,22 +1,22 @@
 # Using netCDF4 datasets in Python
 
 import netCDF4
-import numpy as np
+import numpy as np # Intro to numpy: https://www.youtube.com/watch?v=QUT1VHiLmmI
 
 # This is boilerplate code to open a netCDF file and examine the contents
 # More details at: # https://github.com/Unidata/netcdf4-python/blob/master/examples/reading_netCDF.ipynb
 
 f = netCDF4.Dataset('c3s_obs-sl_glo_phy-ssh_my_twosat-l4-duacs-0.25deg_P1D_1710527666697.nc')
-print(f) 
+# print(f) 
 
 print(f.variables.keys()) # get all variable names
-latitude = f.variables['latitude']
-print(latitude) 
+latitude = f.variables['latitude']; # print(latitude) 
+longitude = f.variables['longitude']; # print(longitude)
+time = f.variables['time']; # print(time)
 
 for d in f.dimensions.items():
     print(d)
     
-latitude.dimensions
-
-latitude.shape
+# latitude.dimensions
+# latitude.shape
 
